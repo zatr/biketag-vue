ce74041165a8caa61430452c494b9bd1d528859e
William Holland <wholland@qualtrics.com>
2024-04-22 22:53:13 -0700

    add player search

--- src/views/Players.vue
+++ src/views/Players.vue
@@ -1,29 +1,34 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <div class="container">
-    <b-pagination
-      v-model="currentPage"
-      :total-rows="totalCount"
-      :per-page="perPage"
-      aria-controls="itemList"
-      align="center"
-      @page-click="changePage"
-    ></b-pagination>
+    <div class="search-bar">
+      <b-form-input type="search" v-model="searchBoxInput" @keyup.enter="performSearch"
+      />
+      <bike-tag-button @click="performSearch">
+        {{ t('pages.players.search') }}
+      </bike-tag-button>
+    </div>
     <div class="player-list">
-      <div v-for="player in playersForList" :key="player.name" class="p-lg-3 p-md-2 mb-2">
+      <div v-for="player in displayedPlayers" :key="player.name" class="p-lg-3 p-md-2 mb-2">
         <player-bicon size="md" :player="player" />
       </div>
     </div>
+    <p v-if="!searchResults.length">
+      {{ t('pages.players.not_found') }}
+    </p>
+    <bike-tag-button v-if="route.name === 'Player Search'" @click="clearSearch">
+      {{ t('pages.players.exit_search') }}
+    </bike-tag-button>
     <b-form-group>
-      <select v-model="perPage" class="form-select mb-2 m-auto" @change="resetCurrentPage">
-        <option v-for="i in 3" :key="Math.pow(10, i)" :value="Math.pow(10, i)">
-          {{ Math.pow(10, i) }}
+      <select v-model="perPage" class="form-select mb-2 m-auto" @change="handlePerPageSelect">
+        <option v-for="opt in perPageOptions" :key="opt" :value="opt">
+          {{ opt }}
         </option>
       </select>
     </b-form-group>
     <b-pagination
       v-model="currentPage"
-      :total-rows="totalCount"
+      :total-rows="searchResults.length"
       :per-page="perPage"
       aria-controls="itemList"
       align="center"
@@ -33,43 +38,115 @@
 </template>
 
 <script setup name="PlayersView">
-import { ref, computed, watch } from 'vue'
-import { useRouter, useRoute } from 'vue-router'
-import { useBikeTagStore } from '@/store/index'
+import { useBikeTagStore } from '@/store/index';
+import { computed, onMounted, ref } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
 
 // components
-import PlayerBicon from '@/components/BikeTagPlayer.vue'
+import BikeTagButton from '@/components/BikeTagButton.vue';
+import PlayerBicon from '@/components/BikeTagPlayer.vue';
+import { useI18n } from 'vue-i18n';
 
 // data
+const perPageOptions = ["10","100","1000"]
+const defaultQuery = {perPage: perPageOptions[0]}
 const router = useRouter()
 const route = useRoute()
-const currentPage = ref(route.params?.currentPage.length ? parseInt(route.params?.currentPage) : 1)
-const perPage = ref(10)
+const currentPage = ref(route.query?.currentPage ? parseInt(route.query?.currentPage) : 1)
+const perPage = ref(route.query?.perPage ? parseInt(route.query?.perPage) : defaultQuery.perPage)
+const searchString = ref(route.query?.searchString || "")
+const searchBoxInput = ref(searchString.value)
 const store = useBikeTagStore()
+const { t } = useI18n()
+
+onMounted( async () => {
+  /* validate query params */
+  if (!perPageOptions.includes(String(perPage.value))) {
+    perPage.value = defaultQuery.perPage
+    router.replace({query: {...route.query, perPage: perPage.value}})
+    .then(resetPageNumber)
+  }
+  else if (isNaN(currentPage.value) ||
+      currentPage.value !== 1 && displayedPlayers.value.length === 0) {
+    resetPageNumber()
+  }
+})
 
 // computed
 const getPlayers = computed(() => store.getPlayers)
-const playersForList = computed(() =>
-  getPlayers.value.slice(
+const searchResults = computed(() => {
+  if (!searchString.value) {
+    return getPlayers.value
+  } else {
+    const lowerQuery = searchString.value.toLowerCase()
+    const startingWithSearchString = []
+    const containingSearchString = []
+    for (let player of getPlayers.value) {
+      const lowerName = player.name.toLowerCase()
+      if (lowerName.startsWith(lowerQuery)) {
+        startingWithSearchString.push(player)
+      } else if (lowerName.includes(lowerQuery)) {
+        containingSearchString.push(player)
+      }
+    }
+    return sortedByName(startingWithSearchString).concat(sortedByName(containingSearchString))
+  }
+})
+const displayedPlayers = computed(() =>
+  searchResults.value.slice(
     (currentPage.value - 1) * perPage.value,
     currentPage.value * perPage.value,
   ),
 )
-const totalCount = computed(() => getPlayers.value.length)
 
 // methods
-const resetCurrentPage = () => {
-  currentPage.value = 1
+const changePage = (_event, pageNumber) => {
+  searchBoxInput.value = searchString.value
+  currentPage.value = pageNumber
+  router.push({query: {
+    ...defaultQuery,
+    ...route.query,
+    currentPage: pageNumber
+  }})
 }
-const changePage = (event, pageNumber) => {
-  router.push('/players/' + pageNumber)
+const resetPageNumber = () => {
+  searchBoxInput.value = searchString.value
+  currentPage.value = 1
+  router.replace({query: {
+    ...defaultQuery,
+    ...route.query,
+    currentPage: currentPage.value
+  }})
+}
+const handlePerPageSelect = () => {
+  searchBoxInput.value = searchString.value
+  currentPage.value = 1
+  router.push({query: {
+    ...defaultQuery,
+    ...route.query,
+    perPage: perPage.value,
+    currentPage: currentPage.value
+  }})
+}
+const sortedByName = (list) => {
+  return list.toSorted((a,b) => a.name.localeCompare(b.name))
+} 
+const clearSearch = () => {
+  searchBoxInput.value = ''
+  searchString.value = ''
+  router.push({name: 'Players', query: {perPage: perPage.value}})
+}
+const performSearch = () => {
+  searchString.value = searchBoxInput.value
+  if (!searchString.value) {
+    router.push({name: 'Players', query: {perPage: perPage.value}})
+  } else {
+    router.push({name: 'Player Search', query: {
+      ...defaultQuery,
+      ...route.query,
+      searchString: searchString.value
+    }}).then(resetPageNumber)
+  }
 }
 
-//watch
-watch(
-  () => 'route.params.currentPage',
-  (val) => {
-    currentPage.value = Number(val)
-  },
-)
 </script>
